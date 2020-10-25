f848ded1fea16ed3b8ec4c1ff0c200f06c62fea6
Emmanouil Konstantinidis <hello@manos.im>
2020-10-25 23:56:35 +0000

    Setup release-it
    Add release-it config
    
    Install release-it
    
    Install more release-it things

---
+++ .release-it.js
@@ -0,0 +1,18 @@
+module.exports = {
+  git: {
+    commitMessage: 'chore: release v${version}',
+    tagName: 'v${version}',
+  },
+  npm: {
+    publish: true,
+  },
+  github: {
+    release: true,
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: 'angular',
+      infile: 'CHANGELOG.md',
+    },
+  },
+};
